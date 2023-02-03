7ac2374ec864ab6228413957c7b3da39c842ec9a
Rodrigo Martin Durán <potro_duran96@hotmail.com>
2023-02-03 02:37:50 -0300

    add database models

---
+++ api/src/models/Activity.js
@@ -0,0 +1,30 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define("activity", {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      allowNull: false,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    dificulty: {
+      type: DataTypes.INTEGER,
+      validate: {
+        min: 1,
+        max: 5,
+      },
+      allowNull: false,
+    },
+    duration: {
+      type: DataTypes.STRING,
+    },
+    season: {
+      type: DataTypes.ENUM(summer, autumn, winter, spring),
+    },
+  });
+};
